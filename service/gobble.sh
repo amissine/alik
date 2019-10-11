addae3a6ca65f372a03171dbeeaaef77db9c46a9
Alec Missine <alec_missine@yahoo.com>
2019-10-11 18:04:17 -0400

    Modify util/common.sh

--- service/gobble.sh
+++ service/gobble.sh
@@ -1,21 +1,11 @@
 #!/usr/local/bin/bash
 
-pipe_in_from_remote_feed () {
-  local ssh_client="$HOME/.ssh/client_session_$REMOTE_FEED"
-  local client=$(hostname)
-  local ssh_server=".ssh/server_session_$client"
-
-# TODO sudo chmod 777 /service/feed/log/main/
-
-  ( sleep 3; scp -q $ssh_client $REMOTE_FEED:$ssh_server ) &
-  ssh -R 0:127.0.0.1:22 $REMOTE_FEED '{ cd /service/feed/log/main; \
-    cat <(ls | grep '.s' | xargs cat); \
-    tail -n 999999 -F current; \
-  }' 2>$ssh_client
-}
+. util/common.sh
 
 if [ -n "$REMOTE_FEED" ]; then
+  log REMOTE_FEED $REMOTE_FEED
   pipe_in_from_remote_feed | go run gobble/main.go
+  log DONE
   exit 0
 fi
 
--- util/common.sh
+++ util/common.sh
@@ -4,3 +4,17 @@ LOG_NUM=n44
 log () { # {{{1
   echo `date +%s` $BASHPID $@ >>./syserr
 }
+
+pipe_in_from_remote_feed () {
+  local ssh_client="$HOME/.ssh/client_session_$REMOTE_FEED"
+  local client=$(hostname)
+  local ssh_server=".ssh/server_session_$client"
+
+# TODO sudo chmod 755 /service/feed/log/main/
+
+  ( sleep 3; scp -q $ssh_client $REMOTE_FEED:$ssh_server ) &
+  ssh -R 0:127.0.0.1:22 $REMOTE_FEED '{ cd /service/feed/log/main; \
+    cat <(ls | grep '.s' | xargs cat); \
+    tail -n 999999 -F current; \
+  }' 2>$ssh_client
+}
