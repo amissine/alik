51a5a36561f03aa8f7246bd009bfdcf2d27423a9
Alec Missine <alec_missine@yahoo.com>
2019-10-17 19:05:21 -0400

    Show all XLMUSD trades

--- mf.sh
+++ mf.sh
@@ -3,4 +3,5 @@
 . util/common.sh
 
 REMOTE_FEED="mia-hub"
-pipe_in_from_remote_feed
+#pipe_in_from_remote_feed
+pipe_in_from_current
--- util/common.sh
+++ util/common.sh
@@ -1,3 +1,4 @@
+# Locals {{{1
 LOG_SIZE=s1000000
 LOG_NUM=n44
 
@@ -5,7 +6,20 @@ log () { # {{{1
   echo `date +%s` $BASHPID $@ >>./syserr
 }
 
-pipe_in_from_remote_feed () {
+pipe_in_from_current () { # {{{1
+  local ssh_client="$HOME/.ssh/client_session_$REMOTE_FEED"
+  local client=$(hostname)
+  local ssh_server=".ssh/server_session_$client"
+
+# TODO sudo chmod 755 /service/feed/log/main/
+
+  ( sleep 3; scp -q $ssh_client $REMOTE_FEED:$ssh_server ) &
+  ssh -R 0:127.0.0.1:22 $REMOTE_FEED '{ cd /service/feed/log/main; \
+    tail -n 99999 -F current; \
+  }' 2>$ssh_client
+}
+
+pipe_in_from_remote_feed () { # {{{1
   local ssh_client="$HOME/.ssh/client_session_$REMOTE_FEED"
   local client=$(hostname)
   local ssh_server=".ssh/server_session_$client"
